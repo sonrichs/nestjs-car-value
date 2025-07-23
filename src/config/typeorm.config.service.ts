4be7c4aa8e81336a0532e73bf17f4cf8ecf17772
Rodrigo Cepeda <rodrigo.cepeda.1993@gmail.com>
2025-07-23 19:18:09 -0400

    add production config

--- src/config/typeorm.config.service.ts
+++ src/config/typeorm.config.service.ts
@@ -7,12 +7,33 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) { }
 
   createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
-    return {
-      type: 'sqlite',
-      synchronize: process.env.NODE_ENV === 'test' ? true : false,
-      database: this.configService.get<string>('DB_NAME'),
-      autoLoadEntities: true,
-      migrationsRun: process.env.NODE_ENV === 'test' ? true : false,
-    };
+    switch (process.env.NODE_ENV) {
+      case 'test':
+        return {
+          type: 'sqlite',
+          synchronize: true,
+          database: this.configService.get<string>('DB_NAME'),
+          autoLoadEntities: true,
+          migrationsRun: true,
+        };
+      case 'development':
+        return {
+          type: 'sqlite',
+          synchronize: false,
+          database: this.configService.get<string>('DB_NAME'),
+          autoLoadEntities: true,
+          migrationsRun: false,
+        };
+      case 'production':
+        return {
+          type: 'postgres',
+          synchronize: false,
+          database: this.configService.get<string>('DB_NAME'),
+          autoLoadEntities: true,
+          migrationsRun: false,
+        };
+      default:
+        throw new Error(`Unknown environment: ${process.env.NODE_ENV}`);
+    }
   }
 }
